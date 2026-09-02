b24424234176ac2705a175efcb048651e43317b7
dwu388 <dwu388@gmail.com>
2026-09-02 09:54:42 -0700

    Route production V24 bootstrap through readiness gate

--- v24_bootstrap.bat
+++ v24_bootstrap.bat
@@ -1,4 +1,4 @@
 @echo off
 setlocal
 cd /d "%~dp0"
-.venv\Scripts\python.exe -m profit_taker.axiom_v24 bootstrap --db data\axiom_v24_raw.sqlite %*
+.venv\Scripts\python.exe -m profit_taker.v24_pretraining_bootstrap --db data\axiom_v24_raw.sqlite %*
