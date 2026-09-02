71a36ecd3d32c66bb85f65cbb18f154a3e5f3ab2
dwu388 <dwu388@gmail.com>
2026-09-02 10:00:13 -0700

    Activate reduced first-model runtime in V24 launchers

--- v24_audit_manifest.bat
+++ v24_audit_manifest.bat
@@ -1,4 +1,4 @@
 @echo off
 setlocal
 cd /d "%~dp0"
-.venv\Scripts\python.exe -m profit_taker.v24_contract_runtime_v2 audit-manifest --db data\axiom_v24_raw.sqlite %*
+.venv\Scripts\python.exe -m profit_taker.v24_contract_runtime_v3 audit-manifest --db data\axiom_v24_raw.sqlite %*
