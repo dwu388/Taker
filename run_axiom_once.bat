8f22da247e854b385e9b4cb16d5a5fde4e9ad63b
dwu388 <dwu388@gmail.com>
2026-09-02 09:56:28 -0700

    Route collection launchers through context instrumentation

--- run_axiom_once.bat
+++ run_axiom_once.bat
@@ -3,4 +3,4 @@ setlocal
 cd /d "%~dp0"
 set "RAW_DB=data\axiom_v24_raw.sqlite"
 .venv\Scripts\python.exe -m profit_taker.collection_admin init --db "%RAW_DB%" --purpose v24_production_raw_collection || exit /b 1
-.venv\Scripts\python.exe -m profit_taker.axiom_migrated_runner --db "%RAW_DB%" --once %*
+.venv\Scripts\python.exe -m profit_taker.axiom_collection_context_runner --db "%RAW_DB%" --once %*
